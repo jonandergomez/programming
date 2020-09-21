4e3d34462289f6e5d90697cd714768ec3b33734c
jonandergomez <jon@dsic.upv.es>
2020-09-21 19:52:07 +0200

    modified:   code/misc/mcd.s modified:   code/misc/p1.s modified:   code/misc/p2.s

--- code/misc/p2.s
+++ code/misc/p2.s
@@ -5,6 +5,7 @@
 pow2:
 .LFB0:
 	.cfi_startproc
+	endbr64
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
@@ -31,5 +32,21 @@ pow2:
 	.cfi_endproc
 .LFE0:
 	.size	pow2, .-pow2
-	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
 	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
