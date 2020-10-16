a8100b1c7a5617cdcc40af0565caf43b409e69b0
Jon Ander Gómez Adrián <jon@Quijote.local>
2020-10-16 14:34:51 +0200

    Few changes

--- code/basic/DataTypeConversions2.java
+++ code/basic/DataTypeConversions2.java
@@ -5,7 +5,7 @@ public class DataTypeConversions2
         double minValue = 10.0, maxValue = 20.0;
         int numIntervals = 2;
         double value = 14.9;
-        value=15.0;
+        //value=15.0;
 
         double sizeInterval = (maxValue - minValue) / numIntervals;
         int indexInterval = (int)((value - minValue) / sizeInterval);
@@ -16,6 +16,6 @@ public class DataTypeConversions2
         double a = minValue + indexInterval * sizeInterval;
         double b = minValue + (indexInterval+1) * sizeInterval;
 
-        System.out.printf("which is [%.2f, %.2f[\n", a, b);
+        System.out.printf(" which is [%.2f, %.2f[\n", a, b);
     }
 }
