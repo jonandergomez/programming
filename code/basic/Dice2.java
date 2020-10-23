6926cf748b9d3f053075785b1df993e143b8ccd5
Jon Ander Gómez Adrián <jon@veraw141-27.wi-fi.upv.es>
2020-10-23 12:20:19 +0200

    modified:   code/basic/Dice2.java modified:   code/basic/SphereExample1.java code/basic/CharExample1.java code/basic/CharExample2.java code/basic/CharExample3.java code/basic/Dice.java

--- code/basic/Dice2.java
+++ code/basic/Dice2.java
@@ -1,7 +1,5 @@
-
 import java.util.*;
 import java.io.*;
-
 
 public class Dice2
 {
--- code/basic/SphereExample1.java
+++ code/basic/SphereExample1.java
@@ -1,13 +1,10 @@
 
-
 import java.util.*;
 import java.io.*;
-
 
 public class SphereExample1
 {
     private static Scanner input = new Scanner(System.in).useLocale(Locale.US);
-
 
     public static void main(String [] args)
     {
