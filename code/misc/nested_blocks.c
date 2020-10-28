d8bf312e57735fe4f0529c4fb3b577548900acda
Jon Ander Gómez <jon@dsic.upv.es>
2020-10-28 12:59:55 +0100

    C example

---
+++ code/misc/nested_blocks.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *arg[])
+{
+    int x = 10;
+
+    {
+        int x = 91;
+
+        printf("%d\n", x);
+    }
+    printf("%d\n", x);
+
+    return EXIT_SUCCESS;
+}
