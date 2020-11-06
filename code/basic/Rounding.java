575d2c88f5ad4abe39ff139769c31586fcec526e
Jon Ander Gómez <jon@dsic.upv.es>
2020-11-06 11:53:52 +0100

    Add Rounding

---
+++ code/basic/Rounding.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Rounding
+{
+    public static void main(String [] args)
+    {
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("\n Please, enter a double value:" );
+        double x = input.nextDouble();
+
+        double y = myRound(x, 2);
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.printf("%.2f\n", x);
+    }
+    public static double myRound(double x, int digits)
+    {
+        return Math.round(x*Math.pow(10,digits)) / Math.pow(10,digits);
+    }
+}
