833140bef2a73fbcb1602761f44a7bc30139afc5
Jon Ander Gómez Adrián <jon@dsic.upv.es>
2020-12-07 19:06:56 +0100

    Umm

--- code/loops/Exponential.java
+++ code/loops/Exponential.java
@@ -3,11 +3,9 @@
 import java.util.*;
 import java.io.*;
 
-
-public class Exponential 
+public class Exponential
 {
     private static Scanner input = new Scanner( System.in ).useLocale( Locale.US );
-
 
     public static void main( String [] args )
     {
@@ -15,56 +13,59 @@ public class Exponential
         int    n = 10;
         double epsilon=1.0e-15;
 
-        for( int i=0; i < args.length; i++ ) {
-            if ( args[i].equals( "-x" ) )
-                x = Double.parseDouble( args[i+1] );
-            else if ( args[i].equals( "-n" ) )
-                n = Integer.parseInt( args[i+1] );
+        // this for uses arrays, please, do not worry about arrays
+        for (int i=0; i < args.length; i++) {
+            if (args[i].equals( "-x"))
+                x = Double.parseDouble(args[i+1]);
+            else if ( args[i].equals("-n") )
+                n = Integer.parseInt(args[i+1]);
         }
-        
-        System.out.printf( "%25.17e\n", myExp3(x,epsilon) );
-        System.out.printf( "%25.17e\n", Math.exp(x) );
+
+        //System.out.printf("our computation %25.17e\n", myExp1(x,n));
+        //System.out.printf("our computation %25.17e\n", myExp2(x,n));
+        System.out.printf("our computation %25.17e\n", myExp3(x,epsilon));
+        System.out.printf("CPU computation %25.17e\n", Math.exp(x));
     }
 
-    private static double myExp1( double x, int n )
+    private static double myExp1(double x, int n)
     {
-        double sum = 1.0, num = 1, den = 1.0;
+        double sum = 1.0,
+               num = 1.0,
+               den = 1.0;
 
-        for( int i=1; i <= n; i++ ) {
-            System.out.printf( "%25.17e  %25.17e  %25.17e\n", num, den, sum );
+        for (int i=1; i <= n; i++) {
+            System.out.printf( "i = %4d %25.17e  %25.17e  %25.17e\n", i, num, den, sum );
             num *= x;
             den *= i;
             sum += num/den;
         }
-        
+
         return sum;
     }
-    private static double myExp2( double x, int n )
+    private static double myExp2(double x, int n)
     {
-        if ( x < 0.0 ) return 1.0/myExp2( -x, n );
+        if (x < 0.0) return 1.0/myExp2(-x, n);
 
-        double sum = 1.0, item = 1.0;
+        double sum = 1.0, term = 1.0;
 
-        for( int i=1; i <= n; i++ ) {
-            System.out.printf( " %25.17e  %25.17e\n", item, sum );
-            item *= x/i;
-            sum += item;
+        for (int i=1; i <= n; i++) {
+            System.out.printf("i = %4d %25.17e  %25.17e\n", i, term, sum);
+            term *= x/i;
+            sum += term;
         }
-        
         return sum;
     }
-    private static double myExp3( double x, double epsilon )
+    private static double myExp3(double x, double epsilon)
     {
-        if ( x < 0.0 ) return 1.0/myExp3( -x, epsilon );
+        if (x < 0.0) return 1.0/myExp3(-x, epsilon);
 
-        double sum = 1.0, item = 1.0;
-        int i =0;
-        while( item > epsilon  ) {
-            System.out.printf( "i=%6d    %25.17e  %25.17e\n", ++i, item, sum );
-            item *= x/i;
-            sum += item;
+        double sum = 1.0, term = 1.0;
+        int i = 0;
+        while (term > epsilon) {
+            System.out.printf( "i=%6d    %25.17e  %25.17e\n", ++i, term, sum );
+            term *= x/i;
+            sum += term;
         }
-        
         return sum;
     }
 }
--- code/loops/Figures.java
+++ code/loops/Figures.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.io.*;
 
 
-public class Figures 
+public class Figures
 {
     private static Scanner input = new Scanner( System.in ).useLocale( Locale.US );
 
@@ -27,86 +27,89 @@ public class Figures
         christmas_tree( height, '*', 'O', 0.15F, offset );
     }
 
-    private static void christmas_tree( int height, char symbol1, char symbol2, float bulbs_proportion, int offset )
+    private static void christmas_tree(int height,
+                                       char symbol1,
+                                       char symbol2,
+                                       float bulbs_proportion,
+                                       int offset)
     {
-        int log_width = (2*height+1) / 5;
-        if ( log_width < 1 ) log_width=1;
-        if ( log_width % 2 == 0 ) log_width++;
+        int log_width = (2 * height + 1 ) / 5;
+        if (log_width < 1) log_width=1;
+        if (log_width % 2 == 0) log_width++;
 
-        int log_height = height/3;
-        if ( log_height < 1 ) log_height=1;
-        
-        for( int i=0; i < height; i++ ) {
-            line( offset, ' ' );
-            line( height-i, ' ' );
-            for( int j=0; j < 1+2*i; j++ ) {
-                System.out.printf( "%c", ( Math.random() < bulbs_proportion ? symbol2 : symbol1 ) );
+        int log_height = height / 3;
+        if (log_height < 1) log_height = 1;
+
+        for(int i=0; i < height; i++) {
+            line(offset, ' ');
+            line(height-i, ' ');
+            for(int j=0; j < 1+2*i; j++) {
+                System.out.printf("%c", (Math.random() < bulbs_proportion ? symbol2 : symbol1));
             }
             System.out.println();
         }
-
-        square( log_height, log_width, '#', offset+height-1-log_width/2 );
+        square(log_height, log_width, '#', offset + height - 1 - log_width / 2);
     }
 
-    private static void tree( int height, char symbol, int offset )
+    private static void tree(int height, char symbol, int offset)
     {
-        up_triangle( height, symbol, offset );
+        up_triangle(height, symbol, offset);
 
-        int log_width = (2*height+1) / 5;
-        if ( log_width < 1 ) log_width=1;
-        if ( log_width % 2 == 0 ) log_width++;
+        int log_width = (2 * height + 1 ) / 5;
+        if (log_width < 1) log_width=1;
+        if (log_width % 2 == 0) log_width++;
 
         int log_height = height/3;
         if ( log_height < 1 ) log_height=1;
 
-        square( log_height, log_width, symbol, offset+height-1-log_width/2 );
+        square(log_height, log_width, symbol, offset + height - 1 - log_width / 2);
     }
 
-    private static void square( int height, int width, char symbol, int offset )
+    private static void square(int height, int width, char symbol, int offset)
     {
-        for( int row=0; row < height; row++ ) {
-            line( offset, ' ' );
-            line( width, symbol );
+        for (int row=0; row < height; row++) {
+            line(offset, ' ');
+            line(width, symbol);
             System.out.println();
         }
     }
-    private static void diamond( int height, char symbol, int offset )
+    private static void diamond(int height, char symbol, int offset)
     {
-        if ( height % 2 == 0 ) ++height;
+        if (height % 2 == 0) ++height;
 
-        up_triangle(   height/2+1, symbol, offset );
-        down_triangle( height/2,   symbol, offset+1 );
+          up_triangle(height / 2 + 1, symbol, offset);
+        down_triangle(height / 2,     symbol, offset + 1);
     }
-    private static void up_triangle( int height, char symbol, int offset )
+    private static void up_triangle(int height, char symbol, int offset)
     {
         int white_spaces = height-1;
         int num_symbols = 1;
 
-        for( int row=0; row < height; row++ ) {
-            line( offset, ' ' );
-            line( white_spaces, ' ' );
-            line( num_symbols, symbol );
+        for (int row=0; row < height; row++) {
+            line(offset, ' ');
+            line(white_spaces, ' ');
+            line(num_symbols, symbol);
             System.out.println();
             white_spaces--;
             num_symbols+=2;
         }
     }
-    private static void down_triangle( int height, char symbol, int offset )
+    private static void down_triangle(int height, char symbol, int offset)
     {
         int white_spaces = 0;
         int num_symbols = 2*height-1;
 
-        for( int row=0; row < height; row++ ) {
-            line( offset, ' ' );
-            line( white_spaces, ' ' );
-            line( num_symbols, symbol );
+        for (int row=0; row < height; row++) {
+            line(offset, ' ');
+            line(white_spaces, ' ');
+            line(num_symbols, symbol);
             System.out.println();
             white_spaces++;
             num_symbols-=2;
         }
     }
-    private static void line( int width, char symbol )
+    private static void line(int width, char symbol)
     {
-        for( int i=0; i < width; i++ ) System.out.printf( "%c", symbol );
+        for(int i=0; i < width; i++) System.out.printf("%c", symbol);
     }
 }
