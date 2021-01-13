a9eb5c809cea85c91369f83dc59d6c9a36066ed2
Jon Ander Gómez Adrián <jon@dsic.upv.es>
2021-01-13 01:09:01 +0100

    Rainfal example updated

--- code/arrays/Rainfall.java
+++ code/arrays/Rainfall.java
@@ -7,21 +7,20 @@ public class Rainfall
     private double [][] data;
     private int         year;
 
-    public Rainfall( int year )
+    public Rainfall(int year)
     {
         this.year = year;
 
         this.data = new double [13][];
 
-        for( int month=1; month <= 12; month++ ) {
-            
-            this.data[month] = new double [1+daysPerMonth(month)];
+        for (int month = 1; month <= 12; month++) {
+            this.data[month] = new double [1 + daysPerMonth(month)];
         }
     }
 
     private int daysPerMonth( int m )
     {
-        switch(m) {
+        switch (m) {
             case  1:
             case  3:
             case  5:
@@ -35,13 +34,13 @@ public class Rainfall
             case  9:
             case 11: return 30;
 
-            case  2: return 28 + (leapYear() ? 1 : 0);
+            case  2: return 28 + (isLeapYear() ? 1 : 0);
 
             default:
-                throw new RuntimeException( String.format( "The value %d for month is crazy!", m ) );
+                throw new RuntimeException(String.format("%d is a wrong value for month!", m ));
         }
     }
-    private boolean leapYear()
+    private boolean isLeapYear()
     {
         return ((year % 4) == 0) && ((year % 100) != 0) || ((year % 400) == 0);
     }
@@ -52,12 +51,12 @@ public class Rainfall
         Random r = new Random();
         Random p = new Random();
 
-        for( int month=1; month < data.length; month++ ) {
+        for (int month=1; month < data.length; month++) {
             // This loop is going to be repeated for all the values of month from 1 to 12, both included.
-            for( int day=1; day < data[month].length; day++ ) {
+            for (int day=1; day < data[month].length; day++) {
                 // This loop is going to be repeated for all the values of day from 1 to the last day of the corresponding month, both included.
                 
-                if ( r.nextDouble() < probabilityOfRainingInADay ) {
+                if (r.nextDouble() < probabilityOfRainingInADay) {
                     
                     data[month][day] = 10 + p.nextInt( 100 );
                 }
@@ -72,10 +71,10 @@ public class Rainfall
     {
         StringBuffer sb = new StringBuffer();
 
-        for( int month=1; month < data.length; month++ ) {
-            for( int day=1; day < data[month].length; day++ ) {
-                if ( data[month][day] != 0 ) 
-                    sb.append( String.format( "%04d-%02d-%02d  %8.2f\n", year, month, day, data[month][day] ) );
+        for (int month = 1; month < data.length; month++) {
+            for (int day = 1; day < data[month].length; day++) {
+                if (data[month][day] != 0) 
+                    sb.append(String.format("%04d-%02d-%02d  %8.2f\n", year, month, day, data[month][day]));
             }
         }
 
@@ -86,34 +85,34 @@ public class Rainfall
     {
         double [][] result = new double [data.length][2];        
 
-        for( int month=1; month < data.length; month++ ) {
-            for( int day=1; day < data[month].length; day++ ) {
+        for (int month = 1; month < data.length; month++) {
+            for (int day = 1; day < data[month].length; day++) {
                 result[month][0] += data[month][day];
             }
-            result[month][1] = result[month][0] / (data[month].length-1);
-            result[0][0] += result[month][0]; // We accumulated to the global rainfall the accumulated rainfall of each month
+            result[month][1] = result[month][0] / (data[month].length - 1);
+            result[0][0] += result[month][0]; // accumulates to the global rainfall from the accumulated rainfall per month
         }
-        result[0][1] = result[0][0] / (365 + (leapYear() ? 1 : 0));
+        result[0][1] = result[0][0] / (365 + (isLeapYear() ? 1 : 0));
 
         return result;
     }
 
-    public String [] daysItRainedMoreThan( double threshold )
+    public String [] daysItRainedMoreThan(double threshold)
     {
-        int counter=0;
-        for( int month=1; month < data.length; month++ ) {
-            for( int day=1; day < data[month].length; day++ ) {
-                if ( data[month][day] > threshold ) counter++;
+        int counter = 0;
+        for (int month = 1; month < data.length; month++) {
+            for (int day = 1; day < data[month].length; day++) {
+                if (data[month][day] > threshold) counter++;
             }
         }
 
         String [] result = new String [counter];
         counter=0;
 
-        for( int month=1; month < data.length; month++ ) {
-            for( int day=1; day < data[month].length; day++ ) {
-                if ( data[month][day] > threshold )
-                    result[counter++] = String.format( "%04d-%02d-%02d  %8.2f", year, month, day, data[month][day] );
+        for (int month = 1; month < data.length; month++) {
+            for (int day = 1; day < data[month].length; day++) {
+                if (data[month][day] > threshold)
+                    result[counter++] = String.format("%04d-%02d-%02d  %8.2f", year, month, day, data[month][day]);
             }
         }
 
@@ -124,8 +123,8 @@ public class Rainfall
     {
         double max = -1;
 
-        for( int month=1; month < data.length; month++ ) {
-            for( int day=1; day < data[month].length; day++ ) {
+        for (int month=1; month < data.length; month++) {
+            for (int day=1; day < data[month].length; day++) {
                 max = (data[month][day] > max) ? data[month][day] : max;
             }
         }
@@ -137,19 +136,19 @@ public class Rainfall
     public static Rainfall loadFromFile( String filename )
         throws Exception // This is for ignoring the checked exceptions that can be thrown in this method.
     {
-        Scanner f = new Scanner( new File( filename ) );
+        Scanner f = new Scanner(new File(filename));
         Rainfall rf = null;
 
-        while( f.hasNext() ) {
+        while (f.hasNext()) {
             String line = f.nextLine().trim();
             String [] components = line.split( "\\s+" );
             String [] date = components[0].split( "-" );
-            int year = Integer.parseInt( date[0] );
-            int month = Integer.parseInt( date[1] );
-            int day = Integer.parseInt( date[2] );
-            double rainfall = Double.parseDouble( components[1] );
+            int year = Integer.parseInt(date[0]);
+            int month = Integer.parseInt(date[1]);
+            int day = Integer.parseInt(date[2]);
+            double rainfall = Double.parseDouble(components[1]);
 
-            if ( rf == null ) rf = new Rainfall( year );
+            if (rf == null) rf = new Rainfall(year);
 
             rf.data[month][day] = rainfall;
         }
@@ -157,37 +156,36 @@ public class Rainfall
         return rf;
     }
 
-    public String findSubsquenceOfAtLeastThreeDaysItRainedMoreThan( int threshold )
+    public String findSubsquenceOfAtLeastThreeDaysItRainedMoreThan(int threshold)
     {
-        int starting_month=-1,
-            starting_day=-1,
-            ending_month=-1,
-            ending_day=-1,
-            counter=0;
+        int starting_month = -1,
+            starting_day = -1,
+            ending_month = -1,
+            ending_day = -1,
+            counter = 0;
 
-        String res="";
+        String res = "";
 
-        for( int month=1; month < data.length; month++ ) {
-            for( int day=1; day < data[month].length; day++ ) {
+        for (int month = 1; month < data.length; month++) {
+            for (int day=1; day < data[month].length; day++) {
 
                 if ( data[month][day] > threshold ) {
 
-                    if ( ++counter == 1 ) {
+                    if (++counter == 1) {
                         starting_month = month;
                         starting_day = day;
                     }
+
                     ending_month = month;
                     ending_day = day;
 
                 } else {
-                
-                    if ( counter >= 3 ) {
-                    
-                        res += String.format( "Subsequence starting at %02d/%02d and ending at %02d/%02d\n",
-                                                              starting_day, starting_month, ending_day, ending_month );
+                    if (counter >= 3) {
+                        res += String.format( "Subsequence starting at %04d-%02d-%02d and ending at %04d-%02d-%02d\n",
+                                                                       year, starting_month, starting_day,
+                                                                                               year, ending_month, ending_day );
                     }
-
-                    starting_month=starting_day=ending_month=ending_day=-1; // Optionall, but just in case!
+                    starting_month = starting_day = ending_month = ending_day = -1;
                     counter=0;
                 }
             }
@@ -196,6 +194,6 @@ public class Rainfall
         if ( res.length() > 0 )
             return res;
         else
-            return String.format( "No more than two consecutive days it rained more than %d liters/m2", threshold );
+            return String.format("No more than two consecutive days it rained more than %d litres/m2", threshold);
     }
 }
--- code/arrays/TestRainfall.java
+++ code/arrays/TestRainfall.java
@@ -14,8 +14,8 @@ public class TestRainfall
     public static void main( String [] args )
         throws Exception
     {
-        File f = new File( "bobo.txt" );
-        System.out.println( "The current working directory is " + f.getAbsoluteFile().getParent() );
+        File f = new File("bobo.txt");
+        System.out.println("The current working directory is " + f.getAbsoluteFile().getParent());
 
 
         Rainfall rf = null;
@@ -24,17 +24,17 @@ public class TestRainfall
 
         do {
             option = menu();
-            switch( option ) {
+            switch (option) {
                 case 1:
                     int year = 0;
-                    System.out.print( "\n Which year? " );
+                    System.out.print("\n Which year? ");
                     year = input.nextInt();
                     rf = new Rainfall( year );
                     rf.randomFill();
                     break;
                 case 2:
                     System.out.println( rf.toString() );
-                    System.out.print( "\n Press ENTER to continue ... " );
+                    System.out.print("\n Press ENTER to continue ... ");
                     input.nextLine();
                     break;
 
@@ -44,43 +44,43 @@ public class TestRainfall
                     break;
 
                 case 4:
-                    System.out.print( "\n\t Please, enter the threshold in liters/m2: " );
+                    System.out.print("\n\t Please, enter the threshold in litres/m2: ");
                     threshold = input.nextInt();
-                    String [] result2 = rf.daysItRainedMoreThan( threshold );
-                    for( int i=0; i < result2.length; i++ ) {
-                        System.out.println( result2[i] );
+                    String [] result2 = rf.daysItRainedMoreThan(threshold);
+                    for (int i = 0; i < result2.length; i++) {
+                        System.out.println(result2[i]);
                     }
-                    System.out.print( "\n Press ENTER to continue ... " );
+                    System.out.print("\n Press ENTER to continue ... ");
                     input.nextLine();
                     break; 
 
                 case 5:
                     double maxRainfall = rf.maxRainfall();
-                    String [] result3 = rf.daysItRainedMoreThan( maxRainfall - 1.0e-5 );
-                    for( int i=0; i < result3.length; i++ ) {
-                        System.out.println( result3[i] );
+                    String [] result3 = rf.daysItRainedMoreThan(maxRainfall - 1.0e-5);
+                    for (int i = 0; i < result3.length; i++) {
+                        System.out.println(result3[i]);
                     }
-                    System.out.print( "\n Press ENTER to continue ... " );
+                    System.out.print("\n Press ENTER to continue ... ");
                     input.nextLine();
                     break;
 
                 case 6:
-                    System.out.print( "\n\t Please, enter the threshold in liters/m2: " );
+                    System.out.print("\n\t Please, enter the threshold in litres/m2: ");
                     threshold = input.nextInt();
-                    String res = rf.findSubsquenceOfAtLeastThreeDaysItRainedMoreThan( threshold );
-                    System.out.println( "\n\n" + res + "\n" );
+                    String res = rf.findSubsquenceOfAtLeastThreeDaysItRainedMoreThan(threshold);
+                    System.out.println("\n\n" + res + "\n");
                     break;
                     
 
                 case 11:
-                    System.out.print( "\n\t Enter the filename: " );
+                    System.out.print("\n\t Enter the filename: ");
                     String filename = input.next();
-                    rf = Rainfall.loadFromFile( filename );
+                    rf = Rainfall.loadFromFile(filename);
                     break;
 
                 case 12:
-                    PrintWriter pw = new PrintWriter( String.format( "rainfall-%d.txt", rf.getYear() ) );
-                    pw.println( rf.toString() );
+                    PrintWriter pw = new PrintWriter(String.format("rainfall-%d.txt", rf.getYear()));
+                    pw.println(rf.toString());
                     pw.close();
                     break;
 
@@ -89,49 +89,49 @@ public class TestRainfall
         } while( option != 0 );
     }
 
-    private static void reportAveragesAndAccumulated( double [][] result )
+    private static void reportAveragesAndAccumulated(double [][] result)
     {
-        System.out.println( "                      Accumulated -       Average " );
-        System.out.println( "  Month           -     rainfall  -       rainfall " );
-        System.out.println( "-----------------------------------------------------" );
-        for( int month = 1; month <= 12; month++ ) {
-            System.out.printf( " %-15s  -  %10.2f   -   %10.2f\n",
-                    monthName[month], result[month][0], result[month][1] );
+        System.out.println("                      Accumulated -       Average ");
+        System.out.println("  Month           -     rainfall  -       rainfall ");
+        System.out.println("-----------------------------------------------------");
+        for (int month = 1; month <= 12; month++) {
+            System.out.printf(" %-15s  -  %10.2f   -   %10.2f\n",
+                    monthName[month], result[month][0], result[month][1]);
         }
-        System.out.println( "-----------------------------------------------------" );
-        System.out.printf( " %-15s  -  %10.2f   -   %10.2f\n",
-                "All the year", result[0][0], result[0][1] );
+        System.out.println("-----------------------------------------------------");
+        System.out.printf(" %-15s  -  %10.2f   -   %10.2f\n",
+                "All the year", result[0][0], result[0][1]);
     }
     private static int menu()
     {
         int option = -1;
         boolean valid_option=false;
         do {
-            System.out.println( "\n\n ----- MENU -----\n" );
-            System.out.println( "\t 1. Create the data structure." );
-            System.out.println( "\t 2. Show the contents." );
-            System.out.println( "\t 3. Average all months and global average." );
-            System.out.println( "\t 4. Days it rained more than ... " );
-            System.out.println( "\t 5. Dates it rained the most. " );
-            System.out.println( "\t 6. Subsequence of days it rained more than ... " );
+            System.out.println("\n\n ----- MENU -----\n");
+            System.out.println("\t 1. Create the data structure.");
+            System.out.println("\t 2. Show the contents.");
+            System.out.println("\t 3. Average all months and global average.");
+            System.out.println("\t 4. Days it rained more than ... ");
+            System.out.println("\t 5. Dates it rained the most. ");
+            System.out.println("\t 6. Subsequence of days it rained more than ... ");
             System.out.println();
-            System.out.println( "\t 11. Load from a file. " );
-            System.out.println( "\t 12. Store to a file. " );
+            System.out.println("\t 11. Load from a file. ");
+            System.out.println("\t 12. Store to a file. ");
             System.out.println();
-            System.out.println( "\t 0. STOP " );
+            System.out.println("\t 0. STOP ");
 
-            System.out.print( "\n\n\t\t Choose an option: " );
+            System.out.print("\n\n\t\t Choose an option: ");
             option = input.nextInt();
             input.nextLine();
 
-            if ( 0 <= option && option <= 12 ) {
-                valid_option=true;
+            if (0 <= option && option <= 12) {
+                valid_option = true;
             } else {
-                valid_option=false;
-                System.err.println( "\n\n SORRY! You entered and invalid option!" );
+                valid_option = false;
+                System.err.println("\n\n SORRY! You entered and invalid option!");
             }
 
-        } while( !valid_option );
+        } while(!valid_option);
         
         return option;
     }
