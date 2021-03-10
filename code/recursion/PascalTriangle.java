5ca732de67b31d2160e92c20a1ee57cca32fab8f
Jon Ander Gómez Adrián <jon@dsic.upv.es>
2021-03-10 09:24:52 +0100

    Updated recursion examples

--- code/recursion/PascalTriangle.java
+++ code/recursion/PascalTriangle.java
@@ -6,15 +6,30 @@ public class PascalTriangle
 {
     public static void main(String [] args)
     {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; i++)
-                System.out.print(" " + pascalTriangle(i, j));
+        int n = 6;
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-n"))
+                n = Integer.parseInt(args[i+1]);
+        }
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++)
+                System.out.printf("  %5d", pascalTriangle(row, col));
             System.out.println();
         }
     }
 
+    /*
+        pt(r, c) = 1 if c == 1 or c == r
+        pt(r, c) = pt(r-1, c-1) + pt(r-1, c) otherwise
+    */
     private static int pascalTriangle(int row, int col)
     {
-
+        if (col == 1  ||  col == row) {
+            return 1;
+        } else {
+            return pascalTriangle(row - 1, col - 1) + pascalTriangle(row - 1, col);
+        }
     }
 }
---
+++ code/recursion/Reverse.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Reverse
+{
+    public static void main(String [] args)
+    {
+        int n = 12345678;
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-n"))
+                n = Integer.parseInt(args[i + 1]);
+        }
+
+        reverse(n);
+    }
+
+    /*
+        print(n) if n < 10
+        print(n % 10) ; reverse(n / 10)  if n >= 0
+    */
+    private static void reverse(int n)
+    {
+        if (n < 10) {
+            System.out.println(n);
+        } else {
+            System.out.print(n % 10); // forward path
+            reverse(n / 10); // recursive call
+        }
+    }
+}
