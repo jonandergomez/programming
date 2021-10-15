892dbd07ef484b573e8af23af9ac073df62eb5f6
Jon Ander Gómez <jon@dsic.upv.es>
2021-10-15 14:27:30 +0200

    Few updates

---
+++ code/basic/TestPoint.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TestPoint
+{
+    public static void main(String [] args)
+    {
+        Point p1 = new Point(7, 8);
+        Point p2 = new Point(1, 2);
+
+        System.out.println("p1: " + p1);
+        System.out.println("p2: " + p2);
+    }
+}
