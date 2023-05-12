56357dc2f93a14adea49da5e22fad9b30f5de0d6
Jon Ander Gómez <jon@dsic.upv.es>
2023-05-12 14:13:29 +0200

    Linked Sequences for non-working students

---
+++ code/linear-data-structures/src/LinkedSequences.java
@@ -0,0 +1,68 @@
+public class LinkedSequences
+{
+    public static NodeInt insert(int value, NodeInt first, NodeInt previous)
+    {
+        if (first == null) {
+            first = new NodeInt(value);
+        } else if (previous != null) {
+            previous.setNext(new NodeInt(value, previous.getNext()));    
+        } else {
+            return new NodeInt(value, first);
+        }
+        return first;
+    }
+    public static NodeInt append(int value, NodeInt first)
+    {
+        if (first == null)
+            return new NodeInt(value);
+        else {
+            NodeInt previous = first;
+            NodeInt current = previous.getNext();
+            while (current != null) {
+                previous = current;
+                current = current.getNext();
+            }
+            previous.setNext(new NodeInt(value));
+            return first;
+        }
+    }
+
+    public static NodeInt remove(NodeInt first, NodeInt previous)
+    {
+        if (first == null) throw new Error("Patan, eres un patan");
+
+        if (previous != null) {
+            if (previous.getNext() != null)
+                previous.setNext(previous.getNext().getNext());
+        } else {
+            first = first.getNext();
+        }
+
+        return first;
+    }
+
+    public static NodeInt removeLastOne(NodeInt first)
+    {
+        if (first == null) throw new Error("Patan, eres un patan");
+
+        NodeInt previous = null;
+        NodeInt current = first;
+        while (current != null && current.getNext() != null) {
+            previous = current;
+            current = current.getNext();
+        }
+        if (previous != null) previous.setNext(null);
+        else first = null;
+
+        return first;
+    }
+
+    public static void main(String [] args)
+    {
+        NodeInt first = null;
+
+        first = LinkedSequences.insert(5, first);
+        first = LinkedSequences.insertAtTheEnd(5, first);
+        first = LinkedSequences.insertAtTheBeginning(5, first);
+    }
+}
