ccdb965c0fa60437d4acf21fe9d528da9118a37d
jonandergomez <jon@dsic.upv.es>
2024-11-22 11:57:30 +0100

    Debug in PrimeFactors

--- code/loops/PrimeFactors.java
+++ code/loops/PrimeFactors.java
@@ -22,11 +22,14 @@ public class PrimeFactors
 
     private static void computeAndShow(int n) {
 
+        boolean debug = true;
+
         System.out.println("\n Prime factor decomposition of integer " + n);
         int e = 0;
         while (n > 0 && (n % 2) == 0) {
             e++;
             n /= 2;
+            if (debug) System.out.printf("f = %d   n = %d   e = %d\n", 2, n, e);
         }
         if (e > 0) {
             System.out.printf("%12d^%d\n", 2, e);
@@ -38,6 +41,7 @@ public class PrimeFactors
             while (n > 0 && (n % f) == 0) {
                 e++;
                 n /= f;
+                if (debug) System.out.printf("f = %d   n = %d   e = %d\n", f, n, e);
             }
             if (e > 0) {
                 System.out.printf("%12d^%d\n", f, e);
